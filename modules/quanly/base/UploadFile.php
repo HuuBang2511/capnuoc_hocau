2dbe9693abda5feade90e4ad792c19062e4c8afe
son146003 <zznhapzz@gmail.com>
2026-06-16 15:30:38 +0700

    a

--- modules/quanly/base/UploadFile.php
+++ modules/quanly/base/UploadFile.php
@@ -13,20 +13,20 @@ class UploadFile extends Model
     public function rules()
     {
         return [
-            [['fileupload'], 'file', 'skipOnEmpty' => true, 'maxFiles' => 10, 'maxSize' => 1024 * 1024 * 20],
-            [['imageupload'], 'file', 'skipOnEmpty' => true, 'extensions' => 'png,jpg,jpeg', 'maxFiles' => 10, 'maxSize' => 1024 * 1024 * 3],
+            [['fileupload'], 'file', 'skipOnEmpty' => true, 'maxFiles' => 10, 'maxSize' => 1024 * 1024 * 500],
+            [['imageupload'], 'file', 'skipOnEmpty' => true, 'extensions' => 'png,jpg,jpeg', 'maxFiles' => 10, 'maxSize' => 1024 * 1024 * 50],
             [['type'], 'string'],
         ];
     }
 
-    public function uploadFile($path, $file){
-        if(file_exists($path)){
-            $file->saveAs($path.$file->baseName . '.' . $file->extension);
+    public function uploadFile($path, $file)
+    {
+        if (file_exists($path)) {
+            $file->saveAs($path . $file->baseName . '.' . $file->extension);
             return true;
-        }
-        else{
+        } else {
             mkdir($path, 0777, true);
-            $file->saveAs($path.$file->baseName . '.' . $file->extension);
+            $file->saveAs($path . $file->baseName . '.' . $file->extension);
             return true;
         }
     }
